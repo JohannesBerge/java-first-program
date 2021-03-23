74a2a9e2b87ce7c923fc1d991634a71c783f1484
JohannesBerge <johannes.berge@accenture.com>
2021-03-23 08:53:45 +0100

    Module 1  part of 2

---
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -0,0 +1,38 @@
+package com.h2;
+
+import java.time.LocalDate;
+
+public class SavingsCalculator {
+    private float[] credits;
+    private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits){
+        this.credits = credits;
+        this.debits = debits;
+    }
+
+    private float sumOfCredits(){
+        float sum = 0.0f;
+
+        for(float credit : credits){
+            sum += credit;
+        }
+        return sum;
+    }
+
+    private float sumOfDebits(){
+        float sum = 0.0f;
+
+        for(float debit : debits){
+            sum += debit;
+        }
+        return sum;
+    }
+
+    private static int remainingDaysInMonth(LocalDate date){
+
+        return -1;
+    }
+
+
+}
